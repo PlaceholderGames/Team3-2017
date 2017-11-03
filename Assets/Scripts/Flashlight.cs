e7f9c3e679663ad5fcc97919f7ff0a697b59a02a
brynFAMOUS <14035693@students.southwales.ac.uk>
2017-11-03 12:56:25 +0000

    Flashlight
    Added working Flashlight with on/off function,
    removed test plane and added "Human" model

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if(GetComponent<Light>().enabled == false)
+            {
+                GetComponent<Light>().enabled = true;
+            }
+            else
+            {
+
+                GetComponent<Light>().enabled = false;
+            }
+        }
+	}
+}
