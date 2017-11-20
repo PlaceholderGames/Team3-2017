3fa9e0e952785b8cdc08f11b5507306a587eb4b2
Callum Griffiths <14040859@students.southwales.ac.uk>
2017-11-20 15:24:15 +0000

    Door Side
    Door Side Scripting added, need to add user interaction with doors now

---
+++ Assets/Scripts/UserInteract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInteract : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+// https://www.youtube.com/watch?v=xcWlXRESo-8
