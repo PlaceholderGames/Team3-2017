76213b70d8fa4926ef4c88e21e9a8c6c42f634ed
Callum Griffiths <14040859@students.southwales.ac.uk>
2017-12-08 12:51:54 +0000

    Walking stuff
    WE DID IT

--- Assets/Scripts/FootSteps.cs
+++ Assets/Scripts/FootSteps.cs
@@ -13,10 +13,28 @@ public class FootSteps : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (cc.isGrounded == true && cc.velocity.magnitude > 2f)
+		if (cc.isGrounded == true &&  Input.GetKeyDown(KeyCode.W))
         {
             GetComponent<AudioSource>().Play();
         }
+        if (cc.isGrounded == true && Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if (cc.isGrounded == true && Input.GetKeyDown(KeyCode.S))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if (cc.isGrounded == true && Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        if (Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.S) ==false && Input.GetKey(KeyCode.D) == false)
+         {
+            GetComponent<AudioSource>().Stop();
+        }
+       
+    }
 
-	}
+
 }
