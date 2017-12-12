c9245b34a558fd89c2956727ff3f4beb2e2263d0
AaronHopkins <aaronhopkins@live.co.uk>
2017-12-12 11:38:03 +0000

    Monster Pathfinding
    i have been trying to get the monster to move to points that i assign as a path but it still needs to be looked into

--- Assets/Scripts/Monster/MonsterMovement.cs
+++ Assets/Scripts/Monster/MonsterMovement.cs
@@ -4,9 +4,50 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour {
 
+    public Transform[] points;
+    private int destPoint = 0;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+
+        /*   // Returns if no points have been set up
+           if (points.Length == 0)
+               return;
+               */
+      
+
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+    /*
     Transform player;
     UnityEngine.AI.NavMeshAgent nav;
-
 
     void Awake()
     {
@@ -18,5 +59,5 @@ public class MonsterMovement : MonoBehaviour {
     void Update()
     {
         nav.SetDestination(player.position);
-    }
+    }*/
 }
