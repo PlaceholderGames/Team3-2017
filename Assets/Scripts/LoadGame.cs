778d78a2e6b74b1a19e8e1d3aac99da5d83fcdbb
Callum Griffiths <14040859@students.southwales.ac.uk>
2018-02-18 13:25:03 +0000

    Credits and Story Menu
    Added a screen for Credits, and A Brief Story screen before loading into the game

--- Assets/Scripts/LoadGame.cs
+++ Assets/Scripts/LoadGame.cs
@@ -7,7 +7,22 @@ public class LoadGame : MonoBehaviour {
 
 	public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowStory()
+    {
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
