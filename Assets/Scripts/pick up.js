dc2ad09684d26131c701940861840fe811a19270
brynFAMOUS <14035693@students.southwales.ac.uk>
2018-04-15 16:43:09 +0100

    Key pick up added

---
+++ Assets/Scripts/pick up.js
@@ -0,0 +1,57 @@
+﻿//Script made by Strups Productions//
+
+var itemInCamera : GameObject; 
+var itemOnTheGround : GameObject; 
+var pickUpSound : AudioClip;
+var text : GameObject;
+var icon : GameObject;
+var activateTrigger : boolean = false; 
+var moveTrigger : boolean = false;
+
+function Start () {
+	text.SetActive(false);	//Disables the text.
+	itemInCamera.SetActive(false);	//Disables the the item in camera.
+	itemOnTheGround.SetActive(true);	//turns on the item on the ground.
+	icon.SetActive(false);	//Disables the icon.
+	activateTrigger = true;	//Activates the Trigger 
+}
+
+
+function OnTriggerExit () {
+
+	activateTrigger = false;	//Disables the Trigger 
+}
+
+function OnTriggerEnter () 
+	{
+	activateTrigger = true;
+
+	if(Input.GetKeyDown(KeyCode.E))
+	{
+		activateTrigger = true;
+		moveTrigger = true;
+
+
+	}
+}
+
+function Update () 
+{
+	if (activateTrigger && Input.GetKey(KeyCode.E))
+	{
+		itemInCamera.SetActive(true); 
+		itemOnTheGround.SetActive(false);
+		icon.SetActive(true);
+		activateTrigger = false;
+		moveTrigger = true;
+		AudioSource.PlayClipAtPoint(pickUpSound, transform.position);
+
+
+		 
+	}
+		if (moveTrigger) 
+	{
+		transform.Translate(Vector3(0,0,1));
+	}
+}
+
---
+++ Assets/Scripts/text.js
@@ -0,0 +1,33 @@
+﻿//Script made by Strups Productions//
+var text : GameObject;
+var activateText : boolean = false; 
+
+function Start () {
+	text.SetActive(false);	//Disabels the text. 
+}
+
+
+function OnTriggerExit () 
+{
+	text.SetActive(false);	//Disabels the text.
+}
+
+function OnTriggerEnter () 
+{
+text.SetActive(true);
+
+if(Input.GetKeyDown(KeyCode.E))
+	{
+		activateText = true;
+	}
+
+}
+
+function Update () 
+{
+	if (activateText && Input.GetKey(KeyCode.E))
+	{
+		 text.SetActive(true);
+	}
+		}
+			
