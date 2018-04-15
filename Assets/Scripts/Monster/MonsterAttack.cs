093f336e9db699b566fbef301432a3f7bad94531
AaronHopkins <aaronhopkins@live.co.uk>
2018-04-15 16:57:46 +0100

    PlayerHelth, MonsterAttack and hiding kind of works
    have added the player health and the monster attack that i should not have pushed to this bransh but no bransh is working for me other that this one so :) the hidding now is able to click on the hiding place and it change the camera but the display dose not change

---
+++ Assets/Scripts/Monster/MonsterAttack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 1;
+
+    GameObject Player;
+    PlayerHealth PlayerHealth;
+
+    bool PlayerInRange;
+    float Timer;
+
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerHealth = Player.GetComponent<PlayerHealth>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            PlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            PlayerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer >= timeBetweenAttacks && PlayerInRange)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        Timer = 0f;
+
+        if(PlayerHealth.CurrntHeath > 0)
+        {
+            PlayerHealth.TakeDamage(attackDamage);
+        }
+    }
+
+}
