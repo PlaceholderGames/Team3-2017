a9c65939f834edd0e3a7e6f8f2fda453cc346723
AaronHopkins <aaronhopkins@live.co.uk>
2018-04-18 11:30:28 +0100

    Better Monster Movement
    add ing a better line of sight for the monster that i lost but redone

---
+++ Assets/Scripts/Monster/MonsterCam.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterCam : MonoBehaviour {
+
+    [SerializeField] //SerializeField means that you can see the variable in the Inspector within Unity, without it being public.
+    Camera cam;
+
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    NavMeshAgent nav;
+
+    public float viewDistance = 10f;
+
+	// Use this for initialization
+	void Start () {
+		if(cam == null)
+            cam = this.GetComponentInChildren<Camera>(); //finds camera if you haven't set it yourself
+
+        if (player == null)
+            player = GameObject.Find("First Person Controller (1)"); //change to the players name if ever changed.
+
+        if (nav == null)
+            nav = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(I_Can_See(player))//checks if the camera is rendering the player
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, player.transform.position, out hit, viewDistance))
+            {
+                if(hit.transform.gameObject == player)
+                {
+                    //move towards the player
+                    nav.SetDestination(player.transform.position);
+                }
+                else
+                {
+
+                }
+            }
+        }
+	}
+
+    private bool I_Can_See(GameObject Object)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        if (GeometryUtility.TestPlanesAABB(planes, Object.GetComponent<Collider>().bounds))
+            return true;
+        else
+            return false;
+    }
+}
