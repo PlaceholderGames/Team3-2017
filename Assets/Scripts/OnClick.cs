4c3a9fd4699eb5710f6487ab077cc5a35eb3c9f5
brynFAMOUS <14035693@students.southwales.ac.uk>
2018-04-18 12:28:07 +0100

    added code

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnClick : MonoBehaviour
+{
+    public UnityEvent MouseDown;
+    private void OnMouseDown()
+    {
+        MouseDown.Invoke();
+    }
+}
